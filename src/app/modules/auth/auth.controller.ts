bbb580a34909113ec4ee0975bbe740098587de70
Israfil Hossen <sisrafilss@gmail.com>
2025-08-20 07:50:09 +0600

    logout, refresh-token, and cange-password auth api added

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,16 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
+import { JwtPayload } from "jsonwebtoken";
 import passport from "passport";
 import AppError from "../../errorHelpers/AppError";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
 import { setAuthCookie } from "../../utils/setCookie";
 import { createUserTokens } from "../../utils/userTokens";
+import { AuthServices } from "./auth.service";
 
 const credentialsLogin = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    console.log(req.body);
-
     passport.authenticate("local", async (err: any, user: any, info: any) => {
       if (err) return next(new AppError(401, err));
       if (!user) return next(new AppError(401, info.message));
@@ -34,6 +34,75 @@ const credentialsLogin = catchAsync(
   }
 );
 
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    console.log("Hitting Log out route!");
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged Out Successfully",
+      data: null,
+    });
+  }
+);
+
+const getNewAccessToken = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      throw new AppError(
+        httpStatus.BAD_REQUEST,
+        "No refresh token recieved from cookies"
+      );
+    }
+    const tokenInfo = await AuthServices.getNewAccessToken(
+      refreshToken as string
+    );
+
+    setAuthCookie(res, tokenInfo);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "New Access Token Retrived Successfully",
+      data: tokenInfo,
+    });
+  }
+);
+
+const changePassword = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newPassword = req.body.newPassword;
+    const oldPassword = req.body.oldPassword;
+    const decodedToken = req.user;
+    await AuthServices.changePassword(
+      oldPassword,
+      newPassword,
+      decodedToken as JwtPayload
+    );
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "Password Changed Successfully",
+      data: null,
+    });
+  }
+);
+
 export const AuthController = {
   credentialsLogin,
+  logout,
+  getNewAccessToken,
+  changePassword,
 };
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,8 +1,32 @@
 import { Router } from "express";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 import { AuthController } from "./auth.controller";
 
 const router = Router();
 
 router.post("/login", AuthController.credentialsLogin);
+router.post("/refresh-token", AuthController.getNewAccessToken);
+router.post("/logout", AuthController.logout);
+router.post(
+  "/change-password",
+  checkAuth(...Object.values(Role)),
+  AuthController.changePassword
+);
+// router.post("/set-password", checkAutvalOh(...Object.values(Role)), AuthControllers.setPassword)
+// router.post("/forgot-password", AuthControllers.forgotPassword)
+// router.post("/reset-password", checkAuth(...Object.values(Role)), AuthControllers.resetPassword)
+
+// // Frontend -> forget-password -> email -> user status check -> short expiration token (valid for 10 min) -> email -> Fronted Link http://localhost:5173/reset-password?email=saminisrar1@gmail.com&token=token -> frontend e  query theke user er email and token extract anbo -> new password user theke nibe -> backend er /reset-password api -> authorization = token -> newPassword -> token verify -> password hash -> save user password
+
+// //  /booking -> /login -> succesful google login -> /booking frontend
+// // /login -> succesful google login -> / frontend
+// router.get("/google", async (req: Request, res: Response, next: NextFunction) => {
+//     const redirect = req.query.redirect || "/"
+//     passport.authenticate("google", { scope: ["profile", "email"], state: redirect as string })(req, res, next)
+// })
+
+// // api/v1/auth/google/callback?state=/booking
+// router.get("/google/callback", passport.authenticate("google", { failureRedirect: `${envVars.FRONTEND_URL}/login?error=There is some issues with your account. Please contact with out support team!` }), AuthControllers.googleCallbackController)
 
 export const AuthRoutes = router;
